6fefac1bfca2471d31497fc5524c80c0c30cef59
Naruto <natsuna@163.com>
2016-02-01 11:29:33 +0800

    AddUserShellVersion02
    通过比对/etc/passwd里面的文件，判断用户名是否已经存在

--- Myadduser.sh
+++ Myadduser.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo "please input username"
 read username
+user=`awk -F : '{print $1}' /etc/passwd | grep "\<$username\>"`
+if [ "$username" == "$user" ]; then
+echo "The user exist"
+exit 0
+fi
 echo "please input password"
 read password
 useradd -d /home/$username $username
 echo $password | passwd --stdin $username
+
