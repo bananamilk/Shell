d246031bdb1f11ff8c609473caf0878020f42443
Naruto <natsuna@163.com>
2016-02-01 15:13:29 +0800

    AddUserMulitShellVersion02
    新增日志文件，添加同名用户时提示用户已存在并输出到日志文件，并跳过此次添加
    报了以下错误“./demo.sh: line 12: $logfile_name: ambiguous redirect”

--- MyadduserM.sh
+++ MyadduserM.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+i=1
+logfile_name=add_user_'date +%y_%m_%d'_00.log
+while [ -f $logfile_name ]; do
+	logfile_name=add_user_'date +%y_%m_%d'_0$i.log
+	let i+=1
+done
 for username in $(cat demo.txt); do
+	user=`awk -F : '{print $1}' /etc/passwd | grep "\<$username\>"`
+	 if [ "$username" == "$user" ]; then
+  		echo "The user $username exists"
+  		echo "User: username creates failure" >> $logfile_name 
+  		continue 
+ 	fi
 	useradd -d /home/$username $username
 	echo demo | passwd --stdin $username
 done
