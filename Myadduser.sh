71a1376042f7ca328880327339514ebfdfaf719a
Naruto <natsuna@163.com>
2016-02-01 15:25:05 +0800

    AddUserShellBugFix01
    把""改为``，不然语句无法运行
    同时把‘\<$username\>’改为"\<$username\>"

--- Myadduser.sh
+++ Myadduser.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "please input username"
 read username
+#user=`cat /etc/passwd | awk -F ':' '{print $1}' | grep "\<$username\>"`
 user=`awk -F : '{print $1}' /etc/passwd | grep "\<$username\>"`
 if [ "$username" == "$user" ]; then
-echo "The user exist"
+echo "The user  exist"
 exit 0
 fi
+	
+
 echo "please input password"
 read password
 useradd -d /home/$username $username
