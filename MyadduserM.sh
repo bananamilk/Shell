83c4b9731cbe6a057550150d29a736c86abed29e
Naruto <natsuna@163.com>
2016-02-02 11:43:40 +0800

    AddUserMulitShellBugFix01
    修复Version02的报错问题
    把date的''改为``

--- MyadduserM.sh
+++ MyadduserM.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+logfile_name=add_users_`date +%Y_%m_%d`_00.log
 i=1
-logfile_name=add_user_'date +%y_%m_%d'_00.log
 while [ -f $logfile_name ]; do
-	logfile_name=add_user_'date +%y_%m_%d'_0$i.log
-	let i+=1
+ 	logfile_name=add_users_`date +%Y_%m_%d`_0$i.log
+ 	let i+=1
 done
 for username in $(cat demo.txt); do
 	user=`awk -F : '{print $1}' /etc/passwd | grep "\<$username\>"`
 	 if [ "$username" == "$user" ]; then
   		echo "The user $username exists"
-  		echo "User: username creates failure" >> $logfile_name 
+  		echo "$user: username creates failure" >> $logfile_name 
   		continue 
  	fi
 	useradd -d /home/$username $username
