3e01a98abbb988dbf4e4ccdf69ff1a7e57bebe90
Naruto <natsuna@163.com>
2016-02-02 14:36:04 +0800

    AddUserMulitShellVersion04
    要求用户输入flag值，如果为true当用户重名时就覆盖
    否则不进行覆盖

--- MyadduserM.sh
+++ MyadduserM.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 logfile_name=add_users_`date +%Y_%m_%d`_00.log
 i=1
+flag=false
 while [ -f $logfile_name ]; do
  	logfile_name=add_users_`date +%Y_%m_%d`_0$i.log
  	let i+=1
 done
+echo "Do you want to overwrite password if user exists (true/false)?"
+read flag
 cat demo.txt | while read line; do
 	username=`echo $line | awk '{print $1}'`
 	password=`echo $line | awk '{print $2}'`
@@ -14,8 +17,13 @@ cat demo.txt | while read line; do
 	fi
 	if [ "$username" == "$user" ]; then
   		echo "The user $username exists"
-  		echo "$user: username creates failure,user exists." >> $logfile_name 
-  		continue 
+  		if [ "$flag" == "true" ]; then
+  			echo "$user: username overwrite." >> $logfile_name 
+  			echo "$username : $password" >> $logfile_name 
+  		else
+  			echo "$user: username creates failure,user exists." >> $logfile_name 
+  			continue 
+  		fi
  	fi
 	useradd -d /home/$username $username
 	echo $password | passwd --stdin $username
