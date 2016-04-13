e9b072bfbdf39fc2ea8bf8005d7a87ee96722cf7
Naruto <natsuna@163.com>
2016-04-13 18:24:18 +0800

    openvas更新添加注释

--- UpdateOpenvas.sh
+++ UpdateOpenvas.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
+echo "openvas-nvt-sync start"
 openvas-nvt-sync
+echo "openvas-nvt-sync finsh"
+
+echo "openvas-scapdata-sync start"
 openvas-scapdata-sync
+echo "openvas-scapdata-sync finsh"
+
+echo "openvas-certdata-sync start"
 openvas-certdata-sync
+echo "openvas-certdata-sync finsh"
+
+echo "openvasmd --rebuild start"
 openvasmd --rebuild  
+echo "openvasmd --rebuild finsh"
+
+echo "openvasmd --update start"
 openvasmd --update
+echo "openvasmd --update finsh"
+
+echo "openvasmd --migrate start"
 openvasmd --migrate
+echo "openvasmd --migrate finsh"
+
+echo "openvassd start"
 openvassd 
+echo "openvassd finsh"
+
+echo "openvasmd start" 
 openvasmd
+echo "openvasmd finsh"
+
+echo "gsad --http-only start"
 gsad --http-only
+echo "gsad --http-only finsh"
 
 logfile_name=update_openvass_`date +%Y_%m_%d`.log
 echo "update succeed" >> $logfile_name 
